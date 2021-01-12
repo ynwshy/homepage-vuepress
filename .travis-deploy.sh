50a801e0bf0bffde9c3090f392857f95c11756b4
huyi <ynwshy@126.com>
2021-01-12 17:05:25 +0800

    config travis

---
+++ .travis-deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+###
+ # @Author: your name
+ # @Date: 2021-01-12 17:03:24
+ # @LastEditTime: 2021-01-12 17:05:09
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: \homepage-vuepress\.travis-deploy.sh
+### 
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd dist/
+
+# 如果是发布到自定义域名
+# echo 'www.zhangyunchen.cc' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+# git push -f https://github.com/ynwshy/ynwshy.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:<USERNAME>/vuepress.git master:gh-pages
+
+
+# 如果使用 travis 持续集成
+git push -f https://${github_personal_access_token}@github.com/ynwshy/ynwshy.github.io.git master:master
+
+cd -
