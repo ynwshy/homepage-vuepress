639cb885dcc53d27e45270e7420d4381d1d438ae
huyi <ynwshy@126.com>
2021-01-12 12:14:38 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:ynwshy/ynwshy.github.io.git master
 git push -f git@github.com:ynwshy/ynwshy.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
